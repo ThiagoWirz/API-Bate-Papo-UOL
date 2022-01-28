3f1991da251fe62d4f20b26c6e92a51640c863a1
Thiago <thiagowirzdemoura@gmail.com>
2022-01-27 22:09:43 -0300

    Request post participants error validation

--- app.js
+++ app.js
@@ -3,58 +3,83 @@ import cors from "cors";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import { MongoClient } from "mongodb";
+import joi from "joi";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
 
-setInterval( async () => {
-    try{
-    const mongoClient = new MongoClient(process.env.MONGO_URI)
-    const connection = await mongoClient.connect()
+setInterval(async () => {
+  try {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+    const connection = await mongoClient.connect();
     const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const messagesCollection = dbBatePapoUOL.collection("messages");
-    const participants = await participantsCollection.find({}).toArray()
+    const participants = await participantsCollection.find({}).toArray();
 
-    for(const participant of participants){
-        if(Date.now() - participant.lastStatus > 10000 ){
-            await participantsCollection.deleteOne({name: participant.name})
-            await messagesCollection.insertOne({ 
-            from: participant.name,
-            to: "Todos",
-            text: "sai da sala...",
-            type: "status",
-            time: dayjs().format("HH:mm:ss"),
-            })
-        }
+    for (const participant of participants) {
+      if (Date.now() - participant.lastStatus > 10000) {
+        await participantsCollection.deleteOne({ name: participant.name });
+        await messagesCollection.insertOne({
+          from: participant.name,
+          to: "Todos",
+          text: "sai da sala...",
+          type: "status",
+          time: dayjs().format("HH:mm:ss"),
+        });
+      }
     }
-    connection.close()}
-    catch (error){
-        console.log(error)
-    }
-}, 15000)
+    connection.close();
+  } catch (error) {
+    console.log(error);
+  }
+}, 15000);
 
 app.post("/participants", async (req, res) => {
-  const participant = { name: req.body.name, lastStatus: Date.now() };
-  const logInMessage = {
-    from: req.body.name,
-    to: "Todos",
-    text: "entra na sala...",
-    type: "status",
-    time: dayjs().format("HH:mm:ss"),
-  };
+  const validation = participantSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+  //   const participant = { name: req.body.name, lastStatus: Date.now() };
+  //   const logInMessage = {
+  //     from: req.body.name,
+  //     to: "Todos",
+  //     text: "entra na sala...",
+  //     type: "status",
+  //     time: dayjs().format("HH:mm:ss"),
+  //   };
   const mongoClient = new MongoClient(process.env.MONGO_URI);
   const connection = await mongoClient.connect();
 
   try {
     const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
+    const participants = await participantsCollection.find({}).toArray();
+    if (participants.find((p) => p.name === req.body.name)) {
+      res.sendStatus(409);
+      return;
+    }
     const messagesCollection = dbBatePapoUOL.collection("messages");
-    await participantsCollection.insertOne(participant);
-    await messagesCollection.insertOne(logInMessage);
+    await participantsCollection.insertOne({
+      name: req.body.name,
+      lastStatus: Date.now(),
+    });
+    await messagesCollection.insertOne({
+      from: req.body.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    });
     res.sendStatus(201);
     connection.close();
   } catch {
@@ -103,22 +128,28 @@ app.post("/messages", async (req, res) => {
 
 app.get("/messages", async (req, res) => {
   const limit = parseInt(req.query.limit);
-  const user = req.header("User")
+  const user = req.header("User");
   const mongoClient = new MongoClient(process.env.MONGO_URI);
   const connection = await mongoClient.connect();
-  
+
   try {
     const dbBatePapoUOL = connection.db("bate-papo-uol");
     const messagesCollection = dbBatePapoUOL.collection("messages");
     const messagesArray = await messagesCollection.find({}).toArray();
-    const invertedMessages = messagesArray.reverse()
-    const filteredMessages = invertedMessages.filter( m => (m.type !== "private_message") || (m.to === user || m.from === user || m.to === "Todos"))
-    if(!limit) {
+    const invertedMessages = messagesArray.reverse();
+    const filteredMessages = invertedMessages.filter(
+      (m) =>
+        m.type !== "private_message" ||
+        m.to === user ||
+        m.from === user ||
+        m.to === "Todos"
+    );
+    if (!limit) {
       res.send(filteredMessages);
       connection.close();
-    } else{
-        res.send(filteredMessages.slice(limit))
-        connection.close();
+    } else {
+      res.send(filteredMessages.slice(limit));
+      connection.close();
     }
   } catch {
     res.sendStatus(500);
@@ -126,30 +157,31 @@ app.get("/messages", async (req, res) => {
   }
 });
 
-app.post("/status", async (req, res) =>{
-    const user = req.header("User")
-    const mongoClient = new MongoClient(process.env.MONGO_URI);
-    const connection = await mongoClient.connect()
+app.post("/status", async (req, res) => {
+  const user = req.header("User");
+  const mongoClient = new MongoClient(process.env.MONGO_URI);
+  const connection = await mongoClient.connect();
 
-    try{
-        const dbBatePapoUOL = connection.db("bate-papo-uol")
-        const participantsCollection = dbBatePapoUOL.collection("participants")
-        const participantsArray = await participantsCollection.find({}).toArray()
-        if(!participantsArray.find( p => p.name === user)){
-            res.sendStatus(404);
-            connection.close()
-        }
-        else{
-           await participantsCollection.updateOne({name: user}, {$set: {lastStatus: Date.now()}})
-            res.sendStatus(200)
-            connection.close()
-        }
+  try {
+    const dbBatePapoUOL = connection.db("bate-papo-uol");
+    const participantsCollection = dbBatePapoUOL.collection("participants");
+    const participantsArray = await participantsCollection.find({}).toArray();
+    if (!participantsArray.find((p) => p.name === user)) {
+      res.sendStatus(404);
+      connection.close();
+    } else {
+      await participantsCollection.updateOne(
+        { name: user },
+        { $set: { lastStatus: Date.now() } }
+      );
+      res.sendStatus(200);
+      connection.close();
     }
-    catch{
-        res.sendStatus(500);
-        connection.close();
-    }
-})
+  } catch {
+    res.sendStatus(500);
+    connection.close();
+  }
+});
 
 app.listen(4000, () => {
   console.log("Rodando em http://localhost:4000");
