b7622b885e0028800ce09f318d0d64f97f82eece
Thiago <thiagowirzdemoura@gmail.com>
2022-01-30 06:43:43 -0300

    Database connection is global now

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import joi from "joi";
 import { stripHtml } from "string-strip-html";
 dotenv.config();
 
+let dbBatePapoUOL;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,11 +22,13 @@ const messageSchema = joi.object({
   type: joi.string().required().valid("message", "private_message"),
 });
 
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+mongoClient.connect(() =>{
+  dbBatePapoUOL = mongoClient.db("bate-papo-uol")
+})
+
 setInterval(async () => {
   try {
-    const mongoClient = new MongoClient(process.env.MONGO_URI);
-    const connection = await mongoClient.connect();
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const messagesCollection = dbBatePapoUOL.collection("messages");
     const participants = await participantsCollection.find({}).toArray();
@@ -42,7 +45,6 @@ setInterval(async () => {
         });
       }
     }
-    connection.close();
   } catch (error) {
     console.log(error);
   }
@@ -56,16 +58,16 @@ app.post("/participants", async (req, res) => {
     res.sendStatus(422);
     return;
   }
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
 
   try {
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const participants = await participantsCollection.find({}).toArray();
-    if (participants.find((p) => p.name === stripHtml(req.body.name).result.trim())) {
+    if (
+      participants.find(
+        (p) => p.name === stripHtml(req.body.name).result.trim()
+      )
+    ) {
       res.sendStatus(409);
-      connection.close();
       return;
     }
     const messagesCollection = dbBatePapoUOL.collection("messages");
@@ -81,26 +83,19 @@ app.post("/participants", async (req, res) => {
       time: dayjs().format("HH:mm:ss"),
     });
     res.sendStatus(201);
-    connection.close();
   } catch {
     res.sendStatus(500);
-    connection.close();
   }
 });
 
 app.get("/participants", async (req, res) => {
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
 
   try {
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const participantsArray = await participantsCollection.find({}).toArray();
     res.send(participantsArray);
-    connection.close();
   } catch {
     res.sendStatus(500);
-    connection.close();
   }
 });
 
@@ -113,16 +108,16 @@ app.post("/messages", async (req, res) => {
     return;
   }
 
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
   try {
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const messagesCollection = dbBatePapoUOL.collection("messages");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const participants = await participantsCollection.find({}).toArray();
-    if (!participants.find((p) => p.name === stripHtml(req.header("User")).result.trim())) {
+    if (
+      !participants.find(
+        (p) => p.name === stripHtml(req.header("User")).result.trim()
+      )
+    ) {
       res.sendStatus(422);
-      connection.close();
       return;
     }
     await messagesCollection.insertOne({
@@ -133,21 +128,16 @@ app.post("/messages", async (req, res) => {
       time: dayjs().format("HH:mm:ss"),
     });
     res.sendStatus(201);
-    connection.close();
   } catch {
     res.sendStatus(500);
-    connection.close();
   }
 });
 
 app.get("/messages", async (req, res) => {
   const limit = parseInt(req.query.limit);
   const user = req.header("User");
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
 
   try {
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const messagesCollection = dbBatePapoUOL.collection("messages");
     const messagesArray = await messagesCollection.find({}).toArray();
     const filteredMessages = messagesArray.filter(
@@ -159,65 +149,50 @@ app.get("/messages", async (req, res) => {
     );
     if (!limit) {
       res.send(filteredMessages);
-      connection.close();
     } else {
       res.send(filteredMessages.slice(-limit));
-      connection.close();
     }
   } catch {
     res.sendStatus(500);
-    connection.close();
   }
 });
 
 app.post("/status", async (req, res) => {
   const user = stripHtml(req.header("User")).result.trim();
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
 
   try {
-    const dbBatePapoUOL = connection.db("bate-papo-uol");
     const participantsCollection = dbBatePapoUOL.collection("participants");
     const participantsArray = await participantsCollection.find({}).toArray();
     if (!participantsArray.find((p) => p.name === user)) {
       res.sendStatus(404);
-      connection.close();
     } else {
       await participantsCollection.updateOne(
         { name: user },
         { $set: { lastStatus: Date.now() } }
       );
       res.sendStatus(200);
-      connection.close();
     }
   } catch {
     res.sendStatus(500);
-    connection.close();
   }
 });
 
 app.delete("/messages/:id", async (req, res) => {
   const id = req.params.id;
   const user = req.header("User");
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
-  const dbBatePapoUOL = connection.db("bate-papo-uol");
   const messagesCollection = dbBatePapoUOL.collection("messages");
   const message = await messagesCollection.findOne({ _id: new ObjectId(id) });
 
   if (!message) {
     res.sendStatus(404);
-    connection.close();
     return;
   }
   if (user !== message.from || message.type === "status") {
     res.sendStatus(401);
-    connection.close();
     return;
   }
   await messagesCollection.deleteOne({ _id: new ObjectId(id) });
   res.sendStatus(200);
-  connection.close();
 });
 
 app.put("/messages/:id", async (req, res) => {
@@ -228,29 +203,32 @@ app.put("/messages/:id", async (req, res) => {
     res.sendStatus(422);
     return;
   }
-  const id = req.params.id
-  const user = req.header("User")
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const connection = await mongoClient.connect();
-  const dbBatePapoUOL = connection.db("bate-papo-uol");
+  const id = req.params.id;
+  const user = req.header("User");
   const messagesCollection = dbBatePapoUOL.collection("messages");
   const participantsCollection = dbBatePapoUOL.collection("participants");
-  const participant = await participantsCollection.findOne({name: user})
+  const participant = await participantsCollection.findOne({ name: user });
   const message = await messagesCollection.findOne({ _id: new ObjectId(id) });
-  
-  if((!participant) || (!message)){
-      res.sendStatus(404)
-      connection.close()
-      return
+
+  if (!participant || !message) {
+    res.sendStatus(404);
+    return;
   }
-  if(message.from !== user){
-      res.sendStatus(401)
-      connection.close()
-      return
+  if (message.from !== user) {
+    res.sendStatus(401);
+    return;
   }
-  await messagesCollection.updateOne({ _id: new ObjectId(id)}, {$set: {to: stripHtml(req.body.to).result.trim(), text: stripHtml(req.body.text).result.trim(), type: stripHtml(req.body.type).result.trim(), time: dayjs().format("HH:mm:ss")}})
-  connection.close()
-  
+  await messagesCollection.updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $set: {
+        to: stripHtml(req.body.to).result.trim(),
+        text: stripHtml(req.body.text).result.trim(),
+        type: stripHtml(req.body.type).result.trim(),
+        time: dayjs().format("HH:mm:ss"),
+      },
+    }
+  );
 });
 app.listen(5000, () => {
   console.log("Rodando em http://localhost:5000");
